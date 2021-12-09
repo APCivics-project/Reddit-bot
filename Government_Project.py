4dd5412a4354c5821d8c26e45fa68ddae1194cea
okkrueger <okthescientist@gmail.com>
2021-12-08 23:15:24 -0700

    Update Government_Project.py
    added more key words

--- Government_Project.py
+++ Government_Project.py
@@ -9,17 +9,19 @@ reddit = praw.Reddit(
 subreddit_List = ["politics","Coronavirus","Conservative","AntiVaxxers"]
 
 
-conservative = ["Conservative","trump"]
-liberal = ["Trumpvirus","democrats"]
+conservative_subs = ["Conservative","trump"]
+liberal_subs = ["Trumpvirus","democrats"]
 other_subs = ['politics',"Coronavirus","AntiVaxxers","news"]
-key_words = ["sterile","microchips","magnetic","mRNA","Covid","vaccine"]
-# https://www.reddit.com/r/AntiVaxxers/comments/ovpntm/the_inventor_of_mrna_technology_speaks_out/
+key_words = ["sterile","microchips","magnetic","mRNA"
+            ,"Covid","corona virus","Covid-19","vaccine"
+            ,"mask","autisim","lockdown","booster"]
+
 def searchSubreddits(sub_list):
     data = [] # data is the following per post: [subreddit,keyphrase found,title,news link]
     for sub in range(len(sub_list)):
         for submission in reddit.subreddit(sub_list[sub]).new(limit=None):
             for words in range(len(key_words)):
-                if key_words[words] in submission.title:
+                if key_words[words].casefold() in submission.title.casefold(): #case fold makes it case insensitive
 
                     if submission.link_flair_text != None:
                         print("flair is" + submission.link_flair_text)
@@ -39,4 +41,7 @@ def searchComment(submission):
             for words in range(len(key_words)):
                 if key_words[words] in comment.body:
                     print(comment.body)
-print(searchSubreddits(liberal))
+print(searchSubreddits(liberal_subs))
+print(searchSubreddits(conservative_subs))
+print(searchSubreddits(other_subs))
+# ADD 0 TO 1 THING OWEN
